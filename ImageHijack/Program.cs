dd96fac1d2a7408d172aab0bee2fb3492259a7a6
Justin Dearing <justin.dearing@live.com>
2019-01-14 12:45:15 -0500

    inital commit

---
+++ ImageHijack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ImageHijack
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            bool hasDoc = args.GetLength(0) > 2;
+            string procToStart = args[0];
+
+            string filePath = string.Empty;
+            if (hasDoc)
+            {
+                for (int i = 2; i < args.GetLength(0); i++)
+                {
+                    if (i > 2)
+                        filePath += " ";
+
+                    filePath += args[i];
+                }       
+            }
+
+            Process p = hasDoc ?
+              Process.Start(procToStart, $"\"{filePath}\"")
+              : Process.Start(procToStart); 
+        }
+    }
+}
